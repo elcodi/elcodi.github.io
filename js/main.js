3552c3614af89ae446d81a9edf25241953fead8e
Miguel Ángel Ríos <ro.miguelangel@gmail.com>
2015-10-05 21:18:53 +0200

    Update demo images

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
             arrows: false,
             infinite: true,
             autoplay: true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 5000,
             fade: true,
             cssEase: 'linear'
         });
